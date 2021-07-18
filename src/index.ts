ad57df3e92d36554d88dae6e33492bf42e9846a5
Dominic <dominicshajaa@icloud.com>
2021-07-18 00:21:00 -0700

    Dependbot alerts fixed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('\x1b[36m%s\x1b[0m', 'In dev mode'); 
   }
   return a + b;
 };
